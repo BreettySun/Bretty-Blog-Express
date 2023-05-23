a42a18267f43adf8b9c5649156ef14cf6e01dc57
BreettySun <2058696174@qq.com>
2023-05-24 00:30:34 +0800

    feat：试错

--- vue-frontend/src/components/Header.vue
+++ vue-frontend/src/components/Header.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="menu-demo">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
-      <a-menu-item key="0" :style="{ padding: 0, marginRight: '38px' }" disabled>
-        <div :style="{
-          width: '80px',
-          height: '30px',
-          borderRadius: '2px',
-          background: 'var(--color-fill-3)',
-          cursor: 'text',
-        }" />
-      </a-menu-item>
+    <a-menu mode="horizontal" :default-selected-keys="['1']" theme="dark" @menuItemClick="handleMenuCLick">
       <a-menu-item key="1">Home</a-menu-item>
       <a-menu-item key="2">Solution</a-menu-item>
       <a-menu-item key="3">Cloud Service</a-menu-item>
@@ -18,13 +9,38 @@
   </div>
 </template>
 <script>
+import { Message } from "@arco-design/web-vue";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
+
+const handleMenuCLick = (key) => {
+  Message.info({ content: `You select ${key}`, showIcon: true });
+  defaultSelectedKeys.value = [key];
+  // router.push({ name: key })
+  console.log(key);
+};
 </script>
 <style lang="scss" scoped>
 .menu-demo {
   box-sizing: border-box;
   width: 100%;
-  // padding: 40px;
-  background-color: var(--color-neutral-2);
+
+  .arco-menu {
+    background-color: #2b333e;
+
+    .arco-menu-item {
+      background-color: rgba($color: #000000, $alpha: 0);
+      // color: var(--color-white);
+
+      .arco-menu-selected {
+
+        .arco-menu-selected-label {
+          background: var(--color-white);
+        }
+      }
+
+    }
+  }
 }
 </style>
--- vue-frontend/src/views/Index.vue
+++ vue-frontend/src/views/Index.vue
@@ -1,17 +1,47 @@
 <template>
-  <div>
-    <header>
-      <Header></Header>
-    </header>
-    <footer>
-      <Footer></Footer>
-    </footer>
+  <div class="layout-demo">
+    <a-layout :style="{
+      height: windowHeight - 1 + 'px'
+    }">
+      <a-layout-header>
+        <Header></Header>
+      </a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+      <a-layout-footer>
+        <Footer></Footer>
+      </a-layout-footer>
+    </a-layout>
   </div>
 </template>
 <script setup>
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
+import { ref } from 'vue'
+
+const windowHeight = window.innerHeight
+
 </script>
-<style lang="">
-  
+<style scoped lang="scss">
+.layout-demo :deep(.arco-layout-header),
+.layout-demo :deep(.arco-layout-footer),
+.layout-demo :deep(.arco-layout-content) {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: var(--color-white);
+  font-size: 16px;
+  font-stretch: condensed;
+  text-align: center;
+}
+
+
+.layout-demo :deep(.arco-layout-header),
+.layout-demo :deep(.arco-layout-footer) {
+  height: 58px;
+  background-color: #2b333e;
+}
+
+.layout-demo :deep(.arco-layout-content) {
+  background-color: #475164;
+}
 </style>
